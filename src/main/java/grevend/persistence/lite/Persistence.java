3ccb5cbb06d0ede463a74182463fea76a25e77dc
grevend <grevend@users.noreply.github.com>
2020-03-02 21:15:24 +0100

    Add Database, Persistence and Main.

---
+++ src/main/java/grevend/persistence/lite/Persistence.java
@@ -0,0 +1,36 @@
+package grevend.persistence.lite;
+
+import grevend.persistence.lite.database.Database;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public final class Persistence {
+
+    private final String name;
+    private final int version;
+    private String user = null, password = null;
+
+    private Persistence(String name, int version) {
+        this.name = name;
+        this.version = version;
+    }
+
+    @Contract(value = "_, _ -> new", pure = true)
+    public static @NotNull Persistence databaseBuilder(String name, int version) {
+        return new Persistence(name, version);
+    }
+
+    public @NotNull Persistence setCredentials(@NotNull String user, @NotNull String password) {
+        this.user = user;
+        this.password = password;
+        return this;
+    }
+
+    public @NotNull Database build() throws IllegalStateException {
+        if (this.user == null || this.password == null) {
+            throw new IllegalStateException("Credentials must be set before building the database.");
+        }
+        return new Database(Database.SQL, "jdbc:postgresql://localhost/", this.name, this.user, this.password, this.version);
+    }
+
+}
---
+++ src/main/java/grevend/persistence/lite/database/Database.java
@@ -0,0 +1,83 @@
+package grevend.persistence.lite.database;
+
+import grevend.persistence.lite.dao.DaoFactory;
+import grevend.persistence.lite.entity.EntityManager;
+import org.jetbrains.annotations.NotNull;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.Properties;
+
+public class Database {
+
+    public static final String SQL = "sql";
+
+    private final String type, url, name, user, password;
+    private final int version;
+
+    public Database(@NotNull String type, @NotNull String url, @NotNull String name, @NotNull String user,
+                    @NotNull String password, int version) {
+        this.type = type;
+        this.url = url;
+        this.name = name;
+        this.user = user;
+        this.password = password;
+        this.version = version;
+    }
+
+    public @NotNull String getType() {
+        return type;
+    }
+
+    public @NotNull String getUrl() {
+        return url;
+    }
+
+    public @NotNull String getName() {
+        return name;
+    }
+
+    public @NotNull String getUser() {
+        return user;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public @NotNull DaoFactory getDaoFactory() {
+        return DaoFactory.getInstance();
+    }
+
+    public @NotNull EntityManager getEntityManager() {
+        return EntityManager.getInstance();
+    }
+
+    public @NotNull Optional<DatabaseMetaData> getMetaData(@NotNull Connection connection) {
+        if (this.type.equals(SQL)) {
+            try {
+                return Optional.ofNullable(connection.getMetaData());
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
+                return Optional.empty();
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Connection createConnection() throws SQLException, IllegalAccessException {
+        if (this.type.equals(SQL)) {
+            Properties props = new Properties();
+            props.setProperty("user", this.user);
+            props.setProperty("password", this.password);
+            return DriverManager.getConnection(this.url + this.name, props);
+        } else {
+            throw new IllegalAccessException("Database type must be SQL to create an sql connection.");
+        }
+    }
+
+}
