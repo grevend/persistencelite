435e6967a75c73307e476d9f18917ee76aeea61b
grevend <grevend@users.noreply.github.com>
2020-03-03 22:45:10 +0100

    Improve Dao.
    Updated Dao, DaoFactory, Postgresql and Persistence.

--- src/main/java/grevend/persistence/lite/dao/DaoFactory.java
+++ src/main/java/grevend/persistence/lite/dao/DaoFactory.java
@@ -1,9 +1,18 @@
 package grevend.persistence.lite.dao;
 
+import grevend.persistence.lite.entity.Attribute;
+import grevend.persistence.lite.sql.PrimaryKey;
+import grevend.persistence.lite.util.Pair;
+import grevend.persistence.lite.util.Triplet;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static grevend.persistence.lite.entity.EntityManager.viableFields;
 
 public final class DaoFactory {
 
@@ -19,35 +28,61 @@ public final class DaoFactory {
         return instance;
     }
 
-    public @NotNull <A> Dao<A, Integer> ofEntity(@NotNull Class<A> entity) throws IllegalArgumentException {
-        return new Dao<>() {
+    public @NotNull <A> List<Triplet<Class<?>, String, String>> getPrimaryKeys(@NotNull Class<A> entity) {
+        return Arrays.stream(entity.getDeclaredFields()).filter(viableFields)
+                .filter(field -> field.isAnnotationPresent(PrimaryKey.class))
+                .map(field -> new Triplet<Class<?>, String, String>(field.getType(), field.getName(),
+                        (field.isAnnotationPresent(Attribute.class) ? field.getAnnotation(Attribute.class).name() :
+                                field.getName()))).collect(Collectors.toList());
+    }
 
-            @Override
-            public boolean create(Collection<A> entities) {
-                return false;
-            }
+    @SuppressWarnings("unchecked")
+    public @NotNull <A, B> Dao<A, B> ofEntity(@NotNull Class<A> entity, @NotNull Class<B> keyClass)
+            throws IllegalArgumentException {
+        var keys = getPrimaryKeys(entity);
+        if (keys.size() <= 0) {
+            throw new IllegalArgumentException(
+                    "Every entity must possess a primary key annotated with " +
+                            PrimaryKey.class.getCanonicalName() + ".");
+        } else {
+            return new Dao<>() {
 
-            @Override
-            public A retrieve(Integer key) {
-                return null;
-            }
+                @Override
+                public boolean createAll(@NotNull Collection<A> entities) {
+                    return false;
+                }
 
-            @Override
-            public List<A> retrieveAll() {
-                return null;
-            }
+                @Override
+                public Optional<A> retrieve(@NotNull B key) {
+                    return Optional.empty();
+                }
 
-            @Override
-            public boolean update(A entity) {
-                return false;
-            }
+                @Override
+                public Optional<A> retrieve(@NotNull Collection<Pair<String, ?>> keyValuePairs) {
+                    return Optional.empty();
+                }
 
-            @Override
-            public boolean delete(A entity) {
-                return false;
-            }
+                @Override
+                public @NotNull List<A> retrieveAll() {
+                    return List.of();
+                }
 
-        };
+                @Override
+                public boolean updateAll(@NotNull Collection<A> entities) {
+                    return false;
+                }
+
+                @Override
+                public boolean deleteAll(@NotNull Collection<A> entities) {
+                    return false;
+                }
+
+            };
+        }
+    }
+
+    public @NotNull <A> Dao<A, Object> ofEntity(@NotNull Class<A> entity) {
+        return ofEntity(entity, Object.class);
     }
 
 }
