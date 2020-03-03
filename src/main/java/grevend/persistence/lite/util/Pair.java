1f2f45b28e3e57e7589dd13b4bd453a271bb0a9b
grevend <grevend@users.noreply.github.com>
2020-03-03 21:42:00 +0100

    Improve Pair.
    Added toPairs stream collector.
    Fixed NullPointerException in toString.

--- src/main/java/grevend/persistence/lite/util/Pair.java
+++ src/main/java/grevend/persistence/lite/util/Pair.java
@@ -3,9 +3,7 @@ package grevend.persistence.lite.util;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -71,6 +69,59 @@ public class Pair<A, B> {
         }
     }
 
+    public static @NotNull <C> Collector<C, ?, List<Pair<C, C>>> toPairs() {
+        return toPairs(false);
+    }
+
+    public static @NotNull <C> Collector<C, ?, List<Pair<C, C>>> toPairs(boolean nullAsPlaceholder) {
+
+        final class Pairing {
+
+            private List<Pair<C, C>> pairs;
+
+            private C first, second;
+            private boolean empty;
+
+            private Pairing() {
+                this.pairs = new ArrayList<>();
+                this.empty = true;
+            }
+
+            public void accept(C value) {
+                if (empty) {
+                    this.first = value;
+                    this.empty = false;
+                } else {
+                    this.second = value;
+                    this.pairs.add(Pair.of(first, second));
+                    this.first = null;
+                    this.second = null;
+                    this.empty = true;
+                }
+            }
+
+            @NotNull Pairing combine(@NotNull Pairing other) {
+                if (!other.empty) {
+                    this.accept(other.first);
+                    this.second = other.second;
+                }
+                return this;
+            }
+
+            public List<Pair<C, C>> finish() {
+                if (nullAsPlaceholder) {
+                    if (this.first != null && this.second == null) {
+                        this.pairs.add(Pair.of(this.first, null));
+                    }
+                }
+                return this.pairs;
+            }
+
+        }
+
+        return Collector.of(Pairing::new, Pairing::accept, Pairing::combine, Pairing::finish);
+    }
+
     public A getA() {
         return a;
     }
@@ -91,8 +142,8 @@ public class Pair<A, B> {
     @SuppressWarnings("unchecked")
     public String toString() {
         return "Pair{"
-                + "a=" + (a.getClass().isArray() ? Arrays.toString((A[]) a) : a)
-                + ", b=" + (b.getClass().isArray() ? Arrays.toString((B[]) b) : b)
+                + "a=" + (a != null && a.getClass().isArray() ? Arrays.toString((A[]) a) : a)
+                + ", b=" + (b != null && b.getClass().isArray() ? Arrays.toString((B[]) b) : b)
                 + '}';
     }
 
